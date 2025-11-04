d16456da6b143e6fc7ccc6ef5c7d199993791ea7
gooberblox <bwrm2009@gmail.com>
2025-11-04 13:29:08 -0500

    Update TripPlanner.java

--- src/main/java/TripPlanner.java
+++ src/main/java/TripPlanner.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class TripPlanner {
@@ -7,7 +8,22 @@ public class TripPlanner {
         // - Average speed (mph)
         // - Fuel efficiency (miles per gallon)
         // - Fuel price per gallon
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter trip distance (miles): ");
+        double dist = scan. nextDouble();
+        System.out.print("Enter average speed (mph): ");
+        double speedAvg = scan.nextDouble();
+        System.out.print("Enter fuel efficiency (miles per gallon): ");
+        double mpg = scan.nextDouble();
+        System.out.print("Enter fuel price per gallon ($): ");
+        double fprice = scan.nextDouble();
+
+        displayResults(
+                calculateTravelTime(speedAvg,dist),
+                calculateFuelNeeded(mpg,fprice,dist),
+                calculateTripCost(fprice,calculateFuelNeeded(mpg,fprice,dist))
+        );
+
         // TODO: Calculate travel time using a return method
         
         // TODO: Calculate fuel needed using a return method
@@ -19,11 +35,17 @@ public class TripPlanner {
     }
     
     // TODO: Implement calculateTravelTime method
-    
+    static double calculateTravelTime(double avgSpeed,double distance) {return distance/avgSpeed;}
     // TODO: Implement calculateFuelNeeded method
-    
+    static double calculateFuelNeeded(double mpg, double price, double dist) {return (dist/mpg);}
     // TODO: Implement calculateTripCost method
-    
+    static double calculateTripCost(double price, double fuelNeeded) {return fuelNeeded*price;}
     // TODO: Implement displayResults method
-    
+    static void displayResults(double travelTime, double fuelNeeded, double tripCost) {
+        DecimalFormat format = new DecimalFormat("0.00");
+        System.out.println("\nResults:");
+        System.out.println("Travel time: "+format.format(travelTime)+" hours");
+        System.out.println("Fuel needed: "+format.format(fuelNeeded)+" gallons");
+        System.out.println("Trip Cost: $"+format.format(tripCost));
+    }
 }
